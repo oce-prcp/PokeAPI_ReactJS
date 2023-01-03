d6d14a61b54bca9008fa5943e595d65e6472d136
PrsJulien <83194514+PrsJulien@users.noreply.github.com>
2023-01-03 14:36:24 +0100

    add to search bar

--- my-pokeapi/src/components/layout/MyPokedex.js
+++ my-pokeapi/src/components/layout/MyPokedex.js
@@ -1,43 +1,93 @@
 import React from "react";
 import { Link} from "react-router-dom";
 import "../../style/mypokedex.css";
+import {useEffect,useState } from "react";
+import PokemonThumbnail from "../PokemonThumbnail";
+
 
 function SearchPokemon() {
+  const [pokemon, setPokemon] = useState(null);
+  const [,setLoading] = useState(false);
+  const [txtImput, settxtInput] = useState("");
+  const [,setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchPokemon() {
+
+      setLoading(true);
+      setError(null);
+
+      try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${txtImput}`);
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error(data.message);
+        }
+
+        setPokemon(data);
+      } catch (e) {
+        setError(e);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    if(txtImput !== ""){
+      fetchPokemon();
+    }
+  }, [txtImput]);
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Enter a name.. ex: pikachu"
 
+<div></div>
+      <input
+        type="text" onChange={name =>{
+          settxtInput(name.target.value);
+          console.log(name)
+        }}
+        
+        
+        placeholder="Enter a name.. ex: pikachu"
+        style={input}
         className="input-pokedex"
-      />
+        />
       <Link to="/pokedex">
-        <button className="btn-input">
+        <button style={btn} className="btn-input">
           Search
         </button>
       </Link>
+        {!pokemon === false &&
+        <PokemonThumbnail
+        id={pokemon.id}
+        name={pokemon.name}
+        image={pokemon.sprites.other.dream_world.front_default}
+        type={pokemon.types[0].type.name}
+        type2={pokemon.types[1]?.type.name}
+        />
+        }
     </div>
 
   );
 }
 
-// const btn = {
-//   textDecoration: "none",
-//   padding: "5px",
-//   textAlign: "center",
-//   width: "6rem",
-//   margin: "auto",
-//   marginTop: "2rem",
-// };
+const btn = {
+  textDecoration: "none",
+  padding: "5px",
+  textAlign: "center",
+  width: "6rem",
+  margin: "auto",
+  marginTop: "2rem",
+};
 
-// const input = {
-//   margin: "auto",
-//   marginTop: "1rem",
-//   width: "20rem",
-//   height: "20px",
-//   padding: "3px",
-//   marginLeft: "1rem",
-// };
+const input = {
+  margin: "auto",
+  marginTop: "1rem",
+  width: "20rem",
+  height: "20px",
+  padding: "3px",
+  marginLeft: "1rem",
+};
 
 export default SearchPokemon;
 
