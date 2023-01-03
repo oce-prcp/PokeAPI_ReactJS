f0fddedfcf718fdabbd709d69275eb21f185d8d7
PrsJulien <83194514+PrsJulien@users.noreply.github.com>
2023-01-03 11:19:05 +0100

    next

--- my-pokeapi/src/App.js
+++ my-pokeapi/src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
-
 import Navbar from "./components/layout/Navbar";
 import Pokedex from "./components/Pokedex";
 
