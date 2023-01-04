0f332f49afa77e1e0957f0893e9785c9bc2ea8c0
BlueDragon1310 <94532496+BlueDragon1310@users.noreply.github.com>
2023-01-04 11:14:56 +0100

    api-pokedex

---
+++ myapi/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("API écoute sur le port 3000");
+});
