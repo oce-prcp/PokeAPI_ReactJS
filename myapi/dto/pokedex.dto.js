57cda4c8fb30bdc8e0ee712480928abc4a3c4fe7
PrsJulien <83194514+PrsJulien@users.noreply.github.com>
2023-01-05 16:07:06 +0100

    verif to dto pokedex

--- myapi/dto/pokedex.dto.js
+++ myapi/dto/pokedex.dto.js
@@ -1,32 +1,3 @@
-const pokedex = require("../models/pokedex.model.js");
-
-const CreatePokedex = async (req, res, next) => {
-  try {
-    const user = req.user;
-    if (!pseudo) {
-      res.status(400).send("Aucun pseudo trouvé");
-      return;
-    }
-    const motDePasse = req.body.motDePasse;
-    if (!motDePasse) {
-      res.status(400).send("Aucun pseudo trouvé");
-      return;
-    }
-    const userExist = await utilisateur.exists({ pseudo: pseudo });
-    if (userExist) {
-      res.status(400).send("L'utilisateur existe déjà");
-      return;
-    }
-    return next();
-  } catch (error) {
-    res.status(500).send("Une erreur est survenue côté serveur");
-  }
-};
-
-module.exports = {
-  CreateUtilisateur,
-};
-
 const pokedex = require("../models/pokedex.model.js")
 
 const CreatePokedex = async(req, res,next) => {
@@ -47,3 +18,4 @@ const CreatePokedex = async(req, res,next) => {
 }
 module.exports = {
     CreatePokedex}
+
